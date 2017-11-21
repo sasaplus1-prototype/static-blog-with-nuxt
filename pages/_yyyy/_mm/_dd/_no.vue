74839c8014dc33228a05dcd2c8ba439e1bf5f23a
sasaplus1 <sasaplus1@gmail.com>
2017-11-21 10:26:20 +0900

    temp

---
+++ pages/_yyyy/_mm/_dd/_no.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <div>{{ fullPath }}</div>
+    <div>{{ String(something) }}</div>
+    <h1>yeah</h1>
+  </div>
+</template>
+
+<script>
+  export default {
+    /*await*/ asyncData(context) {
+      const { route } = context;
+
+      return {
+        fullPath: route.fullPath,
+        something: context.something,
+      };
+    },
+  }
+</script>
