dd102f5da5caf0170266f44a1bd5005413494b3e
sasaplus1 <sasaplus1@gmail.com>
2017-11-21 21:02:18 +0900

    temp

--- pages/index.vue
+++ pages/index.vue
@@ -3,6 +3,8 @@
     <h1>Hello</h1>
     <h1>Hello</h1>
     <h1>Hello</h1>
-    <nuxt-link to="/2017/10/10/01">article</nuxt-link>
+    <nuxt-link to="/2017/10/10/01">article01</nuxt-link>
+    <nuxt-link to="/2017/10/10/02">article02</nuxt-link>
+    <nuxt-link to="/2017/10/10/03">article03</nuxt-link>
   </div>
 </template>
