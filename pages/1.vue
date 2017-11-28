c9a4fa62c83887e881eabcbef743754d290debfa
sasaplus1 <sasaplus1@gmail.com>
2017-11-28 10:23:12 +0900

    temp

---
+++ pages/1.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>1</h1>
+    <p>{{ data }}</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    async asyncData(context) {
+      const data = await import('~/assets/1.json');
+
+      return {
+        data,
+      }
+    },
+  }
+</script>
